fc07241fe7bc8927d22e6965cbf9543b1ded3d28
Oscar Pavon <pavon_oscar@outlook.com>
2025-05-24 11:57:00 +0000

    build binutils

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ if [ -d obj ];then
   build_directory=${working_directory}/obj
   src_directory=${working_directory}/src
 
-  musl_directory=${build_directory}/musl
+  musl_directory=${build_directory}
 
   pushd ${src_directory}/musl
   env CFLAGS="$CFLAGS -Os -ffunction-sections -fdata-sections" 
@@ -25,13 +25,25 @@ if [ -d obj ];then
   make -j32 install
 
   popd
+
   echo "############# Setting CC to musl-gcc"
 
-  export CC="${musl_directory}/musl/bin/musl-gcc"
+  export CC="${musl_directory}/bin/musl-gcc"
   
-
-  echo ${build_directory}
+  echo "############# Building Binutils"
+  pushd ${src_directory}/binutils
+  mkdir obj
+  pushd ${src_directory}/binutils/obj
+  ../configure --prefix=${build_directory}/tools \
+             --with-sysroot=${build_directory} \
+             --disable-nls       \
+             --enable-gprofng=no \
+             --disable-werror    \
+             --enable-new-dtags  \
+             --enable-default-hash-style=gnu
+  make -j32
+  make -j32 install
 
 else
-  mkdir obj
+  mkdir -p obj/tools
 fi
